ca076fb0b484e2de05ff96dfcfd3752bf4e007a7
Doménique van Gennip <vangennip@gmail.com>
2017-01-03 21:01:35 +1100

    testing simple while routine
    part of the next ImageHandler get_next() function to avoid repeating or
    seeing same image at same time.

--- test_stuff.py
+++ test_stuff.py
@@ -1,3 +1,5 @@
+import random
+
 test = {
 	'a': 1,
 	'b': 2,
@@ -17,6 +19,8 @@ test['b'] *= 2
 print(test['a'])
 print(test['b'])
 
+# ------
+
 x = [
 	'aaa',
 	'bbb',
@@ -28,3 +32,15 @@ x = [
 
 for idx, item in enumerate(x):
 	print(idx, item)
+
+# ------
+
+r = ['a','b','c','d','e']
+s = ['a','d','f','g','e']
+si = None
+unique = False
+while not unique:
+	si = s[random.randint(0, len(s)-1)]
+	if (si not in r):
+		unique = True
+print (si)
